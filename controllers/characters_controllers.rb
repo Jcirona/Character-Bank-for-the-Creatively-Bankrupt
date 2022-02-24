421e4bace3c97f4f032d323e027abe1242b2f5cf
Jcirona <jcirona@hotmail.com>
2022-02-24 15:05:11 +1100

    Functionally complete

--- controllers/characters_controllers.rb
+++ controllers/characters_controllers.rb
@@ -24,6 +24,32 @@ post '/characters' do
     redirect '/'
 end
 
+post '/randomise_character' do
+    user_id = session["user_id"]
+
+    names = ['chip', 'shell', 'extra#32']
+    random_name = names.sample
+
+    portrait = "https://images.unsplash.com/photo-1605142859862-978be7eba909?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"
+
+    races = ['Human', 'Elf', 'Dwarf', 'Halfling', 'Half-Elf']
+    random_race = races.sample
+
+    player_classes = ['Fighter', 'Barbarian', 'Cleric, Wizard', 'Rogue' , 'Druid']
+    random_class = player_classes.sample
+
+    strengths = ['Heckin Lorge', 'Charismatic', 'Natural Leader', 'Deft', 'Can operate a vending machine']
+    random_strengths = strengths.sample
+
+    flaws = ["Allergic to adventures", "Fear of gold coins", "Double Dips"]
+    random_flaws = flaws.sample
+
+    
+    create_character(user_id, random_name, portrait, random_race, random_class, random_strengths, random_flaws)
+
+    redirect '/'
+end
+
 get '/characters/:id/edit' do
     id = params["id"]
     character = get_character(id)
